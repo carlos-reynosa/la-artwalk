2c2581d801f8105afb1d9268c355a94e65c7cda7
Carlos E. Reynosa-Nunez <raveash9@gmail.com>
2014-08-04 18:32:22 -0700

    Removed debug statement.

--- scripts/ClearCurrentFilterButton.js
+++ scripts/ClearCurrentFilterButton.js
@@ -2,7 +2,6 @@
  * Removes any buttons that represent art characteristic filters within the UI.
  */
 function clearCurrentFilterButtons() {
-    debugger;
     $(".filterButton").fadeOut(150, function () {
         $(this).remove();
     });
