25445a03c9235beed16d180b8e62c698a2eedccb
Carlos E. Reynosa-Nunez <raveash9@gmail.com>
2014-10-29 13:52:48 -0700

    Formatted code and normalized quotes.

--- scripts/SwitchMarkerType.js
+++ scripts/SwitchMarkerType.js
@@ -1,17 +1,14 @@
 define(['GalleryMarkers', 'AppMap', 'AddMarkersToMap'],
     function (galleryMarkers, map, addMarkersToMap) {
-
         return function (type) {
             for (var i = 0; i < galleryMarkers.length; i++) {
-                if (type == "pink") {
+                if (type == 'pink') {
                     galleryMarkers[i].setIcon(galleryMarkers[i].pinkMarkerURL);
-                } else if (type == "blue") {
+                } else if (type == 'blue') {
                     galleryMarkers[i].setIcon(galleryMarkers[i].blueMarkerURL);
                 }
             }
             addMarkersToMap(galleryMarkers, map);
         };
-
-
     });
 
