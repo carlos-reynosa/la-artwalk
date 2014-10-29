8f4c0b14adc2ef7775dc3117358e6ca93bf05675
Carlos E. Reynosa-Nunez <raveash9@gmail.com>
2014-10-29 14:24:43 -0700

    Added comments.

--- scripts/FilterControl.js
+++ scripts/FilterControl.js
@@ -1,3 +1,7 @@
+/**
+ * Returns a reference to the object containing the controls for applying marker filters
+ * and generating pie charts for each marker.
+ */
 define(['domReady!'], function (document) {
 
     var filterControl = document.getElementById('tabs_wrapper');
