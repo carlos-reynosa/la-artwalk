2f4267425eeaaa232097688699e80281bc28023e
Carlos E. Reynosa-Nunez <raveash9@gmail.com>
2014-10-29 14:09:10 -0700

    Added comments.

--- scripts/AddFilterButtons.js
+++ scripts/AddFilterButtons.js
@@ -1,30 +1,39 @@
-define(['jquery', 'GetFilterButton', 'RefreshSelectBox', 'CurrentSelectedFilter'], function ($, getFilterButton, refreshSelectBox, currentSelectedFilter) {
+/**
+ * Defines a function that adds marker filter representations into the left hand controller UI.
+ */
+define(['jquery', 'GetFilterButton', 'RefreshSelectBox', 'CurrentSelectedFilter'],
+    function ($, getFilterButton, refreshSelectBox, currentSelectedFilter) {
+        return function (categories) {
+            //Get the button HTML representation for each filter and add it to the control
+            for (var i = 0; i < categories.length; i++) {
+                getFilterButton(categories[i]).appendTo('#filter-list');
+            }
 
-    return function (categories) {
-        for (var i = 0; i < categories.length; i++) {
-            getFilterButton(categories[i]).appendTo('#filter-list');
-        }
-        $('.color > a').click(function (e) {
-            currentSelectedFilter.setFilterID($(this).parent().parent().parent().attr('id'));
-            var colorDivLinkID = $(this).attr('name');
-            var linkOffSet = $(this).offset();
-            var colorboard = $('#colorboard').children();
-            var leftOffSet = linkOffSet.left;
-            var topOffSet = linkOffSet.top;
-            $('#colorboard').css({
-                'position': 'absolute',
-                'top': topOffSet + 8,
-                'left': leftOffSet,
-                'z-index': 100000
+            //Handle when the color selection area within the filter button is selected
+            $('.color > a').click(function () {
+                currentSelectedFilter.setFilterID($(this).parent().parent().parent().attr('id'));
+
+                var colorDivLinkID = $(this).attr('name');
+                var linkOffSet = $(this).offset();
+                var leftOffSet = linkOffSet.left;
+                var topOffSet = linkOffSet.top;
+
+                $('#colorboard').css({
+                    'position': 'absolute',
+                    'top': topOffSet + 8,
+                    'left': leftOffSet,
+                    'z-index': 100000
+                });
+
+                $('#colorboard').attr('name', colorDivLinkID);
+                $('#colorboard').fadeIn('fast');
             });
-            $('#colorboard').attr('name', colorDivLinkID);
-            $('#colorboard').fadeIn('fast');
-        });
 
-        $('.close-button').click(function () {
-            $(this).parent().parent().remove();
-            refreshSelectBox();
-        });
-    };
+            //Handle what happens when a filter is removed from the control
+            $('.close-button').click(function () {
+                $(this).parent().parent().remove();
+                refreshSelectBox();
+            });
+        };
 });
 
