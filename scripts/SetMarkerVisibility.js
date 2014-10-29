672013392f58aa1cf647ce44b20073463e7854da
Carlos E. Reynosa-Nunez <raveash9@gmail.com>
2014-10-29 13:52:15 -0700

    Formatted code and normalized quotes.

--- scripts/SetMarkerVisibility.js
+++ scripts/SetMarkerVisibility.js
@@ -1,14 +1,13 @@
 define(['MarkerVisibility'], function (markerVisibility) {
-
-    return function setMarkerVisibilityValue(markers, value) {
+    return function (markers, value) {
         var markerType = markers[0].markerType;
-        if (markerType == "gallery") {
+        if (markerType == 'gallery') {
             markerVisibility.gallery = value;
         }
-        else if (markerType == "bar") {
+        else if (markerType == 'bar') {
             markerVisibility.bar = value;
         }
-        else if (markerType == "restaurant") {
+        else if (markerType == 'restaurant') {
             markerVisibility.restaurant = value;
         }
     };
