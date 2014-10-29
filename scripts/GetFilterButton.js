bf19ed540d58e36de4490c6f25275b25172b624a
Carlos E. Reynosa-Nunez <raveash9@gmail.com>
2014-10-29 13:09:41 -0700

    Formatted code and normalized quotes.

--- scripts/GetFilterButton.js
+++ scripts/GetFilterButton.js
@@ -1,7 +1,9 @@
-define(function () {
-
+define(['jquery'],function ($) {
     return function (categoryName) {
-        return $("<li class='filterButton' id=" + categoryName.value + " value='" + categoryName.value + "'><div class='menu'><div id='color' class='color'><A href='#' id='anchor-" + categoryName.value + "' name='" + categoryName.value + "'>&nbsp;</A> </div><div id='text'>" + categoryName.text + "</div><div class='close-button'><a href='#'><img src='images/close_buttom.jpg' width='20' height='20' border='0' /></a></div></div></li>");
+        return $('<li class="filterButton" id=' + categoryName.value + ' value="' + categoryName.value +
+        '"><div class="menu"><div id="color" class="color"><A href="#" id="anchor-' + categoryName.value +
+        '" name="' + categoryName.value + '">&nbsp;</A> </div><div id="text">' + categoryName.text +
+        '</div><div class="close-button"><a href="#"><img src="images/close_buttom.jpg" width="20" height="20" border="0" /></a></div></div></li>');
     };
 });
 
