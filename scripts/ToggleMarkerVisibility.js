3ea1c88c1b01f5e0fb5a444ad95fbfdad8b75a56
Carlos E. Reynosa-Nunez <raveash9@gmail.com>
2014-10-29 13:53:41 -0700

    Formatted code and normalized quotes.

--- scripts/ToggleMarkerVisibility.js
+++ scripts/ToggleMarkerVisibility.js
@@ -1,16 +1,12 @@
 define(['AppMap', 'GetMarkerVisibility', 'ClearMarkers', 'AddMarkersToMap'],
     function (map, getMarkerVisibility, clearMarkers, addMarkersToMap) {
-
-        function ToggleMarkerVisibility(markers) {
+        return function (markers) {
             if (getMarkerVisibility(markers) == true) {
                 clearMarkers(markers);
             }
             else {
                 addMarkersToMap(markers, map);
             }
-        }
-
-
-        return ToggleMarkerVisibility;
+        };
     }
 );
