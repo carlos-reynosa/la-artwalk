ed255d42272995c862b092e4116af3cdef2d53c1
Carlos E. Reynosa-Nunez <raveash9@gmail.com>
2014-10-29 12:43:18 -0700

    Reformatted code.

--- scripts/ChartColors.js
+++ scripts/ChartColors.js
@@ -1,14 +1,13 @@
-define(function(){
-
+define(function () {
     return {
-            COLOR1: 'ffcc33',
-            COLOR2: 'F3891E',
-            COLOR3: '33cccc',
-            COLOR4: 'A4CE3B',
-            COLOR5: 'ff3399',
-            COLOR6: '3399cc',
-            COLOR7: '009966',
-            COLOR8: '993366',
-            COLOR9: '000000'
-        };
+        COLOR1: 'ffcc33',
+        COLOR2: 'F3891E',
+        COLOR3: '33cccc',
+        COLOR4: 'A4CE3B',
+        COLOR5: 'ff3399',
+        COLOR6: '3399cc',
+        COLOR7: '009966',
+        COLOR8: '993366',
+        COLOR9: '000000'
+    };
 });
