4b2d6263d70623b61733650ba9cb0ec5cb8f70a6
Carlos E. Reynosa-Nunez <raveash9@gmail.com>
2014-10-29 13:39:16 -0700

    Formatted code and normalized quotes.

--- scripts/MapControl.js
+++ scripts/MapControl.js
@@ -1,12 +1,6 @@
 define(['domReady!'], function (document) {
-
-
     var mapControlsDiv = document.getElementById('mapControlsWrapper');
-
-
     mapControlsDiv.index = 4;
 
     return mapControlsDiv;
-
-
 });
