88c928df69017802a82b1d5c359857c9f828910b
Carlos E. Reynosa-Nunez <raveash9@gmail.com>
2014-10-29 12:53:53 -0700

    Formatted code.

--- scripts/CurrentSelectedFilter.js
+++ scripts/CurrentSelectedFilter.js
@@ -1,15 +1,11 @@
 define(function () {
-
     return {
-
         id: null,
         setFilterID: function (newId) {
             this.id = newId;
         },
         getFilterID: function () {
-
             return this.id;
         }
-
     };
 });
