b463d05cce39fc7fa0ca93c25d64e8cefe5905de
Carlos E. Reynosa-Nunez <raveash9@gmail.com>
2014-10-29 13:54:13 -0700

    Formatted code and normalized quotes.

--- scripts/UpdateMarkers.js
+++ scripts/UpdateMarkers.js
@@ -1,13 +1,13 @@
 define(['GalleryMarkers', 'BaseMarkerSize', 'GetResizedMarkerSize', 'ChartURL', 'GalleryMarkerData'],
     function (galleryMarkers, baseMarkerSize, getResizedMarkerSize, ChartURL, galleryData) {
-
-
         return function (markerOptions) {
             var imageURL;
             var resizedMarkerSize;
+
             for (var i = 0; i < galleryMarkers.length; i++) {
                 resizedMarkerSize = getResizedMarkerSize(baseMarkerSize, galleryData[i]);
                 imageURL = new ChartURL(galleryData[i], markerOptions, resizedMarkerSize).getChartURL();
+
                 galleryMarkers[i].setIcon(imageURL);
             }
         };
