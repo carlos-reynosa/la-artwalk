541dfdf58778ae60bff5da65e2758892eb5149e2
Carlos E. Reynosa-Nunez <raveash9@gmail.com>
2014-10-29 13:30:16 -0700

    Formatted code and normalized quotes.

--- scripts/InitializeTabListiners.js
+++ scripts/InitializeTabListiners.js
@@ -41,101 +41,100 @@ require(['jquery',
         /**
          * When the controller slider is selected toggle the
          * gallery markers form pie chart to pink and blue icons.
-         *
          */
-        $("#control1Slider").click(function () {
+        $('#control1Slider').click(function () {
 
             clearMarkers(barMarkers);
             clearMarkers(restaurantMarkers);
             clearMarkers(galleryMarkers);
 
-            $("#tabs_container,#tabs_content_container").each(function () {
+            $('#tabs_container,#tabs_content_container').each(function () {
                 $(this).animate({
                     width: 'toggle'
-                }, "0");
+                }, '0');
             });
 
             //If the slider is closed when clicked, open it
-            if ($("#control1Slider").css("marginLeft") == "0px") {
-                map.mapTypeAppState = "pie";
-                $("#control1Slider").css("marginLeft", "245px");
-                $("#control1Slider").css("backgroundImage", "url(./images/slide_btn.jpg)");
+            if ($('#control1Slider').css('marginLeft') == '0px') {
+                map.mapTypeAppState = 'pie';
+                $('#control1Slider').css('marginLeft', '245px');
+                $('#control1Slider').css('backgroundImage', 'url(./images/slide_btn.jpg)');
             }
             else { //Revert to display the pink and blue gallery markers.
 
-                map.mapTypeAppState = "colors"
-                switchMarkerType("pink");
+                map.mapTypeAppState = 'colors'
+                switchMarkerType('pink');
                 addMarkersToMap(barMarkers, map);
                 addMarkersToMap(restaurantMarkers, map);
 
                 for (var i = 0; i < galleryMarkers.length; i++) {
                     if (galleryMarkers[i].isSelected) {
-                        galleryMarkers[i].switchMarkerColor("blue");
+                        galleryMarkers[i].switchMarkerColor('blue');
                     }
                 }
 
-                $("#control1Slider").animate({
+                $('#control1Slider').animate({
                     marginLeft: '0px'
                 });
 
-                $("#control1Slider").css("backgroundImage", "url(./images/slide_btn2.jpg)");
+                $('#control1Slider').css('backgroundImage', 'url(./images/slide_btn2.jpg)');
             }
         });
 
         //The slider is closed by default
-        $("#control1Slider").click();
+        $('#control1Slider').click();
 
         //The pink and blue markers are displayed by default
-        switchMarkerType("pink");
+        switchMarkerType('pink');
         addMarkersToMap(barMarkers, map);
         addMarkersToMap(restaurantMarkers, map);
 
-        $("button2").click(function () {
+        $('button2').click(function () {
             if (markerListControl.isOpen == true) {
                 markerListControl.isOpen = false;
             } else if (tempThis.markerListControl.isOpen == false) {
                 markerListControl.isOpen = true;
             }
-            $('#slide_container').slideToggle("");
+            $('#slide_container').slideToggle('');
         });
 
         //Default art category to be displayed within the filter controller
         currentCategory.setCategory(galleryMetaData.categories.media);
 
         //What the tabs within the UI should do when clicked
-        $("#tabs li").click(function () {
+        $('#tabs li').click(function () {
 
             //Reset the marker view by removing any bubbles and markers currently being displayed
             infoBubble.close();
             clearMarkers(galleryMarkers);
 
-            $("#tabs li").removeClass('active');
-            $(this).addClass("active");
-            $(".tab_content").hide();
+            $('#tabs li').removeClass('active');
+            $(this).addClass('active');
+            $('.tab_content').hide();
 
-            var selected_tab = $(this).find("a").attr("href");
+            var selected_tab = $(this).find('a').attr('href');
 
             //When switching tabs clear any filter currently being viewed
             clearCurrentFilterButtons();
 
-            $("#filter-type-selector").children().each(function (index, element) {
+            $('#filter-type-selector').children().each(function () {
                 $(this).remove();
             });
 
             //Depending on the tab selected, replace the tab with the correct content
-            if (selected_tab == "#tab1") {
+            if (selected_tab == '#tab1') {
                 replaceSelectOptions(galleryMetaData.categories.media.value);
                 currentCategory.setCategory(galleryMetaData.categories.media);
-            } else if (selected_tab == "#tab2") {
+            } else if (selected_tab == '#tab2') {
                 replaceSelectOptions(galleryMetaData.categories.style.value);
                 currentCategory.setCategory(galleryMetaData.categories.style);
-            } else if (selected_tab == "#tab3") {
+            } else if (selected_tab == '#tab3') {
                 replaceSelectOptions(galleryMetaData.categories.price.value);
                 currentCategory.setCategory(galleryMetaData.categories.price);
             }
 
-            $("select").multiselect("refresh");
-            $("#tab1").fadeIn(0);
+            $('select').multiselect('refresh');
+            $('#tab1').fadeIn(0);
 
             return false;
         });
@@ -146,17 +145,16 @@ require(['jquery',
          * The listener adds the color selected within the color board to the
          * buttons on the UI representing each filter.
          */
-        $(".colorBoardItem").click(function (e) {
-
+        $('.colorBoardItem').click(function () {
             //Get the color that was selected within the color board
-            var selectedColor = this.getAttribute("value");
-            var currentListColor = $("#" + currentSelectedFilter.getFilterID() + " div:first-child").children()[0];
+            var selectedColor = this.getAttribute('value');
+            var currentListColor = $('#' + currentSelectedFilter.getFilterID() + ' div:first-child').children()[0];
 
             //Add the color to the button that represents the filter within the UI
-            currentListColor.style.backgroundColor = "#" + chartColors[selectedColor];
-            currentListColor.setAttribute("value", selectedColor);
+            currentListColor.style.backgroundColor = '#' + chartColors[selectedColor];
+            currentListColor.setAttribute('value', selectedColor);
 
-            $("#colorboard").fadeOut(0);
+            $('#colorboard').fadeOut(0);
         });
 
     });
