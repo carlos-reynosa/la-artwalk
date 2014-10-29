e80bbed9f782f815330dfa4b7ef9c3d820d20680
Carlos E. Reynosa-Nunez <raveash9@gmail.com>
2014-10-29 13:10:56 -0700

    Formatted code and normalized quotes.

--- scripts/GetFilterButtonColorData.js
+++ scripts/GetFilterButtonColorData.js
@@ -1,9 +1,8 @@
 define(function () {
-
     return function () {
         var data = [];
-        $(".menu #color").map(function (index, element) {
-            data.push(this.getAttribute("value"));
+        $('.menu #color').map(function () {
+            data.push(this.getAttribute('value'));
         });
         return data;
     };
