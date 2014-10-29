42e68ac0c9619b4ddb34a6eff1e158ee81713ac7
Carlos E. Reynosa-Nunez <raveash9@gmail.com>
2014-10-29 13:51:05 -0700

    Formatted code and normalized quotes.

--- scripts/RestaurantMarkers.js
+++ scripts/RestaurantMarkers.js
@@ -1,18 +1,20 @@
 define(['RestaurantData', 'Google'], function (restaurantData, google) {
     var markerList = [];
+
     for (var i = 0; i < restaurantData.length; i++) {
         markerList.push(new google.maps.Marker({
             position: new google.maps.LatLng(restaurantData[i][3], restaurantData[i][4]),
             clickable: true,
             title: restaurantData[i][0],
             zIndex: restaurantData[i][5],
-            icon: "./images/restaurant.png",
+            icon: './images/restaurant.png',
             shape: {
                 coord: [30, 15, 15],
-                type: "circle"
+                type: 'circle'
             }
         }));
-        markerList[i].markerType = "restaurant";
+
+        markerList[i].markerType = 'restaurant';
         markerList[i].restaurantID = i;
         markerList[i].address = restaurantData[i][1];
         markerList[i].phoneNumber = restaurantData[i][2];
