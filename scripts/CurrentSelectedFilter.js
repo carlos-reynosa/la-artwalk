c51b1e1e4b9a69c1bceb99b5e4aca5297828ab05
Carlos E. Reynosa-Nunez <raveash9@gmail.com>
2014-10-29 14:22:51 -0700

    Added comments.

--- scripts/CurrentSelectedFilter.js
+++ scripts/CurrentSelectedFilter.js
@@ -1,3 +1,6 @@
+/**
+ * Stores the current filter the user is interacting with. This is a helper function for when a user is selecting colors within the colors board.
+ */
 define(function () {
     return {
         id: null,
