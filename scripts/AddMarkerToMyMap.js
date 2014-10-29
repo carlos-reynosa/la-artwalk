134efc41ddf6d1ac4da4b12bf6d479c4348a2c03
Carlos E. Reynosa-Nunez <raveash9@gmail.com>
2014-10-29 12:39:30 -0700

    Normalized quotes.

--- scripts/AddMarkerToMyMap.js
+++ scripts/AddMarkerToMyMap.js
@@ -2,33 +2,33 @@ define(['MarkerInfoBubble','AppMap','jquery','MarkerListControl'],function(infoB
     return  function () {
         var marker = infoBubble.getCurrentMarker();
         if (marker.galleryID >= 0) {
-            if (map.mapTypeAppState == "pie") {
+            if (map.mapTypeAppState == 'pie') {
                 if (!marker.isSelected) {
                     marker.fadeOut();
                 }
             }
-            else if (map.mapTypeAppState == "colors") {
+            else if (map.mapTypeAppState == 'colors') {
                 if (!marker.isSelected) {
-                    marker.switchMarkerColor("blue");
+                    marker.switchMarkerColor('blue');
                 }
             }
         }
         else if (marker.barID >= 0 || marker.restaurantID >= 0) {
-            marker.switchMarkerColor("light-blue");
+            marker.switchMarkerColor('light-blue');
         }
         if (marker.galleryID >= 0) {
-            markerListControl.addMarker(marker, "gallery");
+            markerListControl.addMarker(marker, 'gallery');
             marker.isSelected = true;
         }
         else if (marker.barID >= 0) {
-            markerListControl.addMarker(marker, "bar");
+            markerListControl.addMarker(marker, 'bar');
             marker.isSelected = true;
         }
         else if (marker.restaurantID >= 0) {
-            markerListControl.addMarker(marker, "restaurant");
+            markerListControl.addMarker(marker, 'restaurant');
             marker.isSelected = true;
         }
-        $(".infobubble-button-add").fadeTo("slow", .5);
+        $('.infobubble-button-add').fadeTo('slow', .5);
     };
 });
 
