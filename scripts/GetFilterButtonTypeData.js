e40bfa2368dea4e0f8c4218f43dd94cddfef82d5
Carlos E. Reynosa-Nunez <raveash9@gmail.com>
2014-10-29 13:12:30 -0700

    Formatted code and normalized quotes.

--- scripts/GetFilterButtonTypeData.js
+++ scripts/GetFilterButtonTypeData.js
@@ -1,8 +1,8 @@
 define(['jquery'], function ($) {
     return function () {
         var data = [];
-        $("#filter-list > li").map(function () {
-            data.push(this.getAttribute("value"));
+        $('#filter-list > li').map(function () {
+            data.push(this.getAttribute('value'));
         });
         return data;
     };
